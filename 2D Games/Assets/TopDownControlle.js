ab4659955eb492dae4bbb9039c9f1058187d9998
= <clcarnhem@MBPvanCLCArnhem.mshome.net>
2016-04-10 12:41:53 +0200

    2d Top down game?

---
+++ 2D Games/Assets/TopDownControlle.js
@@ -0,0 +1,31 @@
+﻿ #pragma strict
+ 
+ //Inspector Variables
+   var playerSpeed : float = 10; //speed player moves
+   
+   function Update () 
+   {
+   
+       MoveForward(); // Player Movement 
+   }
+   
+   function MoveForward()
+   {
+   
+       if(Input.GetKey("up"))//Press up arrow key to move forward on the Y AXIS
+       {
+           transform.Translate(0,playerSpeed * Time.deltaTime,0);
+       }
+       if(Input.GetKey("down"))//Press up arrow key to move forward on the Y AXIS
+       {
+           transform.Translate(0,-playerSpeed * Time.deltaTime,0);
+       }
+       if(Input.GetKey("left"))//Press up arrow key to move forward on the Y AXIS
+       {
+           transform.Translate(-playerSpeed * Time.deltaTime,0 ,0);
+       }
+       if(Input.GetKey("right"))//Press up arrow key to move forward on the Y AXIS
+       {
+           transform.Translate(playerSpeed * Time.deltaTime,0 ,0);
+       }
+   }
